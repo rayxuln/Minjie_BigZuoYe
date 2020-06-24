2322718caeed8476c3d1cb7319c02d0a625ba651
Raiix <imbosslx@sina.com>
2020-06-24 16:36:15 +0800

    Init

---
+++ src/main/java/org/minjie/group27/bigzuoye/App.java
@@ -0,0 +1,5 @@
+package org.minjie.group27.bigzuoye;
+
+public class App {
+
+}
---
+++ src/test/java/org/minjie/group27/bigzuoye/AppTest.java
@@ -0,0 +1,13 @@
+package org.minjie.group27.bigzuoye;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class AppTest {
+
+    @Test
+    public void testForTest(){
+        assertTrue(true);
+    }
+
+}
